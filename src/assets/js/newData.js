b49b41ddedc60f08a626df918761e2b748cede17
Carolina Torres Duran <ca.torresduran@gmail.com>
2018-08-27 21:03:46 -0300

    cargando datos desde la api

--- src/assets/js/newData.js
+++ src/assets/js/newData.js
@@ -8,15 +8,17 @@ let userPercent = 0;
 let userProgress = 0; // variable que entra a la propiedad intro para sacar % de lecturas y demases
 
 Promise.all([ // Ejecuta todas las llamadas de manera paralela.
-  fetch('../data/cohorts/lim-2018-03-pre-core-pw/users.json'),
-  fetch('../data/cohorts/lim-2018-03-pre-core-pw/progress.json'),
-  fetch('../data/cohorts.json')
+  fetch('https://api.laboratoria.la/cohorts/lim-2018-03-pre-core-pw/users'),
+  fetch('https://api.laboratoria.la/cohorts/lim-2018-03-pre-core-pw/progress'),
+  fetch('https://api.laboratoria.la/cohorts/')
 ]).then((responses) => { // Responde a todas las promesas.
   return Promise.all(responses.map((response => response.json()))); // traduce el "el texto plano" en JSON
 }).then((data) => { // Arreglo de respuestas en json.
   users = Object.values(data[0]); // se usa values porque id y name son values, si pongo keys sale undefined
   progress = Object.values(data[1]); // se usa values porque la propiedad intro está dentro del key ID
+  console.log(progress)
   cohorts = Object.values(data[2]);
+  console.log(cohorts)
 }).catch(
   () => {
     console.log('fallo fetch');
