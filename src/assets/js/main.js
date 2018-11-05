000f357d5123e43908703994f25725e598bc0b8f
Carolina Torres Duran <ca.torresduran@gmail.com>
2018-11-05 03:17:41 -0300

    arreglando eslint

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,9 +1,8 @@
 function printUsers(data) {
-
-  let tablota = '';
-
-  tablota += `  <table class="highlight white card" id="myTable">
-<thead>
+  let tablahd = '';
+  tablahd +=
+    `<table class="highlight white card" id="myTable">
+    <thead class="grey lighten-3">
     <tr>
         <th>Nombre<i class="material-icons" onclick="sortUsersName('DESC')">arrow_drop_down</i><i class="material-icons" onclick="sortUsersName('ASC')">arrow_drop_up</i></th>
         <th>Lecturas <i class="material-icons" onclick="sortReads('DESC')">arrow_drop_down</i><i class="material-icons" onclick="sortReads('ASC')">arrow_drop_up</i></th>
@@ -11,76 +10,65 @@ function printUsers(data) {
         <th>Quizzes <i class="material-icons" onclick="sortQuizzScore('DESC')">arrow_drop_down</i><i class="material-icons" onclick="sortQuizzScore('ASC')">arrow_drop_up</i></th>
         <th>Total <i class="material-icons" onclick="sortUsersPercent('DESC')">arrow_drop_down</i><i class="material-icons" onclick="sortUsersPercent('ASC')">arrow_drop_up</i></th>
     </tr>
-</thead>
-<tbody id="tabla"></tbody>
-</table>`;
+    </thead>
+    <tbody id="tabla"></tbody>
+    </table>`;
 
-  document.getElementById('tablota').innerHTML = tablota;
+  document.getElementById('tablahd').innerHTML = tablahd;
 
   let tabla = '';
-  data.forEach(function (users) {
+  data.forEach(function(users) {
     if (users.name === '') {
       tabla +=
         `<tr>
-      <td>SIN NOMBRE</td>
-      <td>${users.stats.reads.percent}</td>
-      <td>${users.stats.exercises.percent}</td>
-      <td>${users.stats.quizzes.scoreAvg}</td>
-      <td>${users.stats.percent}</td>
+            <td>SIN NOMBRE</td>
+            <td>${users.stats.reads.percent}</td>
+            <td>${users.stats.exercises.percent}</td>
+            <td>${users.stats.quizzes.scoreAvg}</td>
+            <td>${users.stats.percent}</td>
       </tr>`;
-    }
-
-    else {
+    } else {
       tabla +=
         `<tr>
-      <td>${users.name}</td>
-
-      <td>${users.stats.reads.percent}</td>
-      <td>${users.stats.exercises.percent}</td>
-      <td>${users.stats.quizzes.scoreAvg}</td>
-      <td>${users.stats.percent}</td>
+        <td>${users.name}</td>
+        <td>${users.stats.reads.percent}</td>
+        <td>${users.stats.exercises.percent}</td>
+        <td>${users.stats.quizzes.scoreAvg}</td>
+        <td>${users.stats.percent}</td>
       </tr>`;
 
     }
   });
   document.getElementById('tabla').innerHTML = tabla;
-
 }
-
 
 function printUsersnull() {
-
-
-  document.getElementById("tabla").innerHTML = "Datos no disponibles"
+  document.getElementById('tabla').innerHTML = 'Datos no disponibles';
 }
-
 
 function filterUsers() {
   var input, filter, table, tr, td, i;
-  input = document.getElementById("filtro");
+  input = document.getElementById('filtro');
   filter = input.value.toUpperCase();
-  table = document.getElementById("tabla");
-  tr = table.getElementsByTagName("tr");
+  table = document.getElementById('tabla');
+  tr = table.getElementsByTagName('tr');
   for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
+    td = tr[i].getElementsByTagName('td')[0];
     if (td) {
       if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
+        tr[i].style.display = '';
       } else {
-        tr[i].style.display = "none";
+        tr[i].style.display = 'none';
       }
     }
   }
 }
 
-
 function loaded() {
-
   let tabla = '';
-
-  tabla += `     <div class="progress">
-  <div class="indeterminate"></div>
-</div>`;
-
+  tabla += 
+  `<div class="progress yellow">
+    <div class="indeterminate  yellow darken-3"></div>
+  </div>`;
   document.getElementById('tabla').innerHTML = tabla;
 }
